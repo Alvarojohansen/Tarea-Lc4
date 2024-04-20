c5451e6327f2f77dc6d617f48c7d10b5760fa94a
Alvarojohansen <johansenalvaro@gmail.com>
2024-04-19 23:43:36 -0300

    avance -(controller todoITem)

--- toDolist/toDolist/Controllers/TodoController.cs
+++ toDolist/toDolist/Controllers/TodoController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using toDolist.data;
+using toDolist.Entities;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,6 +11,65 @@ namespace toDolist.Controllers
     [ApiController]
     public class TodoController : ControllerBase
     {
-        
+
+        private readonly TodoContext _context;
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TodoItem>>> GetAllTodoItem()
+        {
+            var allTodoItem = await _context.TodoItems.ToListAsync();
+            return Ok(allTodoItem);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<List<TodoItem>>> GetTodoItemById(int id)
+        {
+            var dbTodoItem = await _context.TodoItems.FindAsync(id);
+
+            if (dbTodoItem is null)
+                return NotFound("todo item not found");
+            return Ok(dbTodoItem);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<TodoItem>>> AddTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+            return Ok(todoItem);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<TodoItem>>> UpdateTodoItem(TodoItem todoItem)
+        {
+            var dbItem = await _context.TodoItems.FindAsync(todoItem.Id_todo_item);
+
+            if (dbItem is null)
+                return NotFound("item not found");
+
+            dbItem.Title = todoItem.Title;
+            dbItem.Description = todoItem.Description;
+            dbItem.Usuario = todoItem.Usuario;
+
+            await _context.SaveChangesAsync();
+            return Ok(dbItem);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult<List<TodoItem>>> DeleteItem(int id)
+        {
+            var dbItem = await _context.TodoItems.FindAsync(id);
+            if (dbItem is null)
+                return NotFound("item not found");
+
+            _context.TodoItems.Remove(dbItem);
+            await _context.SaveChangesAsync();
+            return Ok(dbItem);
+        }
     }
 }
